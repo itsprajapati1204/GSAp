52645fbf71e53fbf9a7cfe123ec8c64d3f451d77
Shubham Singh Prajapati <siddharthsingh75250@gmail.com>
2024-11-03 09:52:21 +0530

    new

---
+++ P5/script.js
@@ -0,0 +1,25 @@
+var intialPath ="M 10 100 Q 500 100 1490 100"
+
+var finalPath = "M 10 100 Q 500 100 1490 100"
+
+var string = document.querySelector("#string")
+
+
+string.addEventListener("mousemove", function(dets){
+  path = `M 10 100 Q ${dets.x} ${dets.y} 1490 100`
+
+  gsap.to("svg path",{
+    attr:{d:path},
+    duration:0.2,
+    ease:"power3.out"
+  })
+})
+
+string.addEventListener("mouseleave",function(){
+  gsap.to("svg path",{
+    attr:{d:finalPath},
+    duration:1.5,
+    ease:"elastic.out(1,0.2)"
+
+  })
+})
